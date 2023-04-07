266bada9b253327fd73d7ac2851b2801013a964c
Wyn Chrsitian <wynchristian.rebanal@tup.edu.ph>
2023-04-07 18:49:57 +0800

    add new class

---
+++ someFile.php
@@ -0,0 +1,21 @@
+<?php
+class Fruit
+{
+  // Property 
+  public $name;
+  public $size;
+  private $price;
+
+  function __construct($name, $size, $price)
+  {
+    $this->name = $name;
+    $this->size = $size;
+    $this->price = $price;
+  }
+}
+
+$durian = new Fruit("Durian", 23, 400);
+$kiwi = new Fruit("Kiwi", 18, 390);
+
+echo $durian->name;
+echo $kiwi->size;
